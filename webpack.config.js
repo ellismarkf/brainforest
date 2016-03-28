7204a415b7f74c8008c3b301b6f2de01efa4554b
Mark F. Ellis II <ellismarkf@gmail.com>
2016-03-27 23:07:19 -0500

    add webpack dev server with HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-	entry: './index.js',
+	entry: {
+		app: [
+			'./index.js',
+		  	"webpack-dev-server/client?http://localhost:8080/",
+		  	"webpack/hot/dev-server"
+		]
+	},
 	output: {
 		path: __dirname + '/dist',
 		filename: 'bundle.js'
 	},
-	loaders: [
-		{ test: /\.js$/, loaders: ['babel'] }
-	],
-
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+		]	
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Mark &amp; Lisy's Wedding",
+			template: 'index-html.ejs',
+			inject: 'body'
+		})
+	]
 }
