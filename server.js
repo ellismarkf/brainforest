c108e6f77f068d2dfb93dbbb418207d9c0c488d4
Mark F. Ellis II <ellismarkf@gmail.com>
2016-03-30 21:59:57 -0500

    use process port variable to serve app

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000
 
+app.set('port', port)
 app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
 	res.send('index.html')
 })
 
-app.listen(8080)
+app.listen(app.get('port'), function() {
+	console.log('express server started & listening on port', app.get('port'))
+})
