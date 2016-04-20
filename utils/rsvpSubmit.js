1857796944fd03984c3a4d7478081c42692ce637
Mark F. Ellis II <ellismarkf@gmail.com>
2016-04-20 00:05:01 -0500

    include and serialize more fields to send to rsvp spreadsheet

--- utils/rsvpSubmit.js
+++ utils/rsvpSubmit.js
@@ -1,13 +1,14 @@
+import { forEach } from 'lodash'
+import serialize from 'form-serialize'
+
 export default function rsvpSubmit(event) {
-	// debugger
-	event.preventDefault();
-	console.log('button clicked!');
-	const inputs = document.querySelectorAll('input[type="text"]');
-	const data = new FormData();
-	for (let i = 0 ; i < inputs.length ; i++) {
-		data.append(inputs[i].placeholder, inputs[i].value)
-	}
-	var xhr = new XMLHttpRequest();
+	event.preventDefault()
+	const form = document.querySelector('form[name="rsvp"]')
+	const data = new FormData()
+	forEach(serialize(form, { hash: true }), (val, key) => {
+		data.append(key, val)
+	})
+	var xhr = new XMLHttpRequest()
 	xhr.open('POST', 'https://script.google.com/macros/s/AKfycbwPOXWkiSb6EGjS6FiBIUYWhsMqkBq9rFOcrjzHi0_3kkfbfWM/exec')
 	xhr.send(data)
 }
